088a0de1195505a51d8499df05ad1e4e9c20a0d4
KevinLaird <kevinlaird16@gmail.com>
2022-11-07 19:29:17 -0800

    Moved board size to state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header></header>
       <main>
-        <TicTacToe boardSize={3} />
+        <TicTacToe />
       </main>
     </div>
   );
--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -3,8 +3,9 @@ import Board from './Board';
 
 import styles from './TicTacToe.module.css';
 
-const TicTacToe = ({ boardSize }) => {
+const TicTacToe = () => {
   const [turn, setTurn] = useState('x');
+  const [boardSize, setBoardSize] = useState(3);
   const [error, setError] = useState('');
 
   return (
@@ -16,6 +17,8 @@ const TicTacToe = ({ boardSize }) => {
       >
         <h1>{error ? error : 'Tic Tac Toe'}</h1>
       </div>
+
+      <input type="text"></input>
       <h1>Turn: {turn}</h1>
       <Board
         boardSize={boardSize}
