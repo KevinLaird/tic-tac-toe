14eb4d0a60fa22d99a9fd4f57ae07e29ada2aca5
KevinLaird <kevinlaird16@gmail.com>
2022-11-08 16:26:30 -0800

    Game now checks for winners

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -5,18 +5,20 @@ import styles from './TicTacToe.module.css';
 
 const TicTacToe = () => {
   const [turn, setTurn] = useState('x');
+  const [winner, setWinner] = useState(null);
   const [board, setBoard] = useState();
   const [error, setError] = useState('');
 
   const boardSizeHandler = (size) => {
-    if (!size) {
+    if (!size || winner) {
+      setWinner(null);
       setBoard(null);
       return;
     }
     const board = Array.from(new Array(size), () => []);
     let count = 0;
     board.forEach((row, rowIndex) => {
-      for (let columnIndex = 0; columnIndex <= size - 1; columnIndex++) {
+      for (let columnIndex = 0; columnIndex < size; columnIndex++) {
         row.push({
           id: count,
           value: '-',
@@ -29,10 +31,72 @@ const TicTacToe = () => {
     setBoard(board);
   };
 
+  const isHorizontalWinner = (symbol, board) => {
+    return board.some((moves) => moves.every((move) => move.value === symbol));
+  };
+
+  const isVerticalWinner = (symbol, board) => {
+    const transposedBoard = board.map((_, index) =>
+      board.map((row) => row[index]),
+    );
+    return transposedBoard.some((moves) =>
+      moves.every((move) => move.value === symbol),
+    );
+  };
+
+  const isDiagonalWinner = (symbol, board) => {
+    const diagonalMoves = [];
+    const equalBasedDiagonal = [];
+    const sumBasedDiagonal = [];
+
+    for (let row = 0; row < board.length; row++) {
+      for (let column = 0; column < board.length; column++) {
+        if (row === column) {
+          equalBasedDiagonal.push(board[row][column]);
+        }
+      }
+    }
+    for (let row = 0; row < board.length; row++) {
+      for (let column = 0; column < board.length; column++) {
+        if (row + column === board.length - 1) {
+          sumBasedDiagonal.push(board[row][column]);
+        }
+      }
+    }
+
+    diagonalMoves.push(equalBasedDiagonal, sumBasedDiagonal);
+
+    return diagonalMoves.some((moves) =>
+      moves.every((move) => move.value === symbol),
+    );
+  };
+
+  const isGameOver = (board) =>
+    board.every((row) => row.every((move) => move.value !== '-'));
+
+  const checkWinner = (turn, board) => {
+    if (isGameOver(board)) {
+      return "It's a tie!";
+    }
+    return (
+      isHorizontalWinner(turn, board) ||
+      isVerticalWinner(turn, board) ||
+      isDiagonalWinner(turn, board)
+    );
+  };
+
   const updateBoardHandler = (rowIndex, columnIndex, turn) => {
     const newBoard = board;
     newBoard[rowIndex][columnIndex].value = turn;
-    console.log(newBoard);
+    const winnerCheck = checkWinner(turn, newBoard);
+    if (winnerCheck === "It's a tie!") {
+      setWinner(winnerCheck);
+      return;
+    }
+    if (checkWinner(turn, newBoard)) {
+      setWinner(turn);
+      return;
+    }
     setBoard(newBoard);
   };
 
@@ -72,8 +136,17 @@ const TicTacToe = () => {
   return (
     <div>
       {header}
-      <h1>Turn: {turn}</h1>
-      {boardJsx}
+
+      {winner && winner !== "It's a tie!" && (
+        <h1>Congratulations! Player {winner} won!</h1>
+      )}
+      {winner === "It's a tie!" && <h1>That's a tie. Nobody wins!</h1>}
+      {!winner && (
+        <>
+          <h1>Turn: {turn}</h1>
+          {boardJsx}
+        </>
+      )}
       <button type="button" onClick={() => boardSizeHandler(null)}>
         reset
       </button>
